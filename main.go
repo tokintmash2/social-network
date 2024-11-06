8a09dbb9d781ede2f4437f3ba1893f6aeaf24e6d
Miikael Volkonski <miikael@volkonski.com>
2024-11-06 14:23:08 +0200

    asd

--- main.go
+++ main.go
@@ -7,17 +7,8 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// var db *sql.DB
-
 func main() {
-	// var err error
-
-	// Connect to the SQLite database
-	// db, err = sql.Open("sqlite3", "database.db")
-	// if err != nil {
-	// 	log.Fatal("Error while connecting to the database:", err)
-	// }
-	// defer db.Close() // Close the database connection when main() exits
+	
 
 	// Initialize the forum database and create necessary tables
 	database.ConnectToDB()
@@ -25,22 +16,5 @@ func main() {
 	// err = database.RunMigrations(database.DB)
 	database.RunMigrations(database.DB)
 
-	// Run migrations
-	// driver, err := sqlite.WithInstance(db, &sqlite.Config{})
-	// if err != nil {
-	// 	log.Fatal("Could not create SQLite driver:", err)
-	// }
-
-	// m, err := migrate.NewWithDatabaseInstance(
-	// 	"file://backend/database/migrations",
-	// 	"sqlite3", driver)
-	// if err != nil {
-	// 	log.Fatal("Could not create migrate instance:", err)
-	// }
-
-	// if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-	// 	log.Fatal("Could not run up migrations:", err)
-	// }
-
-	// log.Println("Migrations applied successfully")
+	
 }
