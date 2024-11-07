f0c137a485a1f871732b5594218b9193ca11fb66
Miikael Volkonski <miikael@volkonski.com>
2024-11-07 09:53:40 +0200

    asd

--- backend/handlers/loginHandler.go
+++ backend/handlers/loginHandler.go
@@ -1,11 +1,11 @@
 package handlers
 
 import (
-
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
+	"social-network/utils"
 )
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
@@ -55,36 +55,64 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	// success, userCookie, userId := validators.ValidateUserLogin(email, password)
 	var callback = make(map[string]string)
 	// if success {
-		sessionCookie := http.Cookie{
-			Name:     "socialNetworkSession",
-			Value:    "userCookie", // testing
-			Expires:  time.Now().Add(time.Minute * 500),
-			Path:     "/",
-			HttpOnly: true,
-			SameSite: http.SameSiteNoneMode,
-			Secure:   true,
-		}
-		http.SetCookie(w, &sessionCookie)
+	sessionCookie := http.Cookie{
+		Name:     "socialNetworkSession",
+		Value:    "userCookie", // testing
+		Expires:  time.Now().Add(time.Minute * 500),
+		Path:     "/",
+		HttpOnly: true,
+		SameSite: http.SameSiteNoneMode,
+		Secure:   true,
+	}
+	http.SetCookie(w, &sessionCookie)
 
-		authCookie := http.Cookie{
-			Name:     "socialNetworkAuth",
-			Value:    "true",
-			Expires:  time.Now().Add(time.Minute * 500),
-			Path:     "/",
-			SameSite: http.SameSiteNoneMode,
-			Secure:   true,
-		}
-		http.SetCookie(w, &authCookie)
-		callback["login"] = "success"
-		callback["userid"] = "userId"
-		callback["useravatar"] = "validators.ValidateUserAvatar(userId)"
-		callback["useremail"] = "validators.ValidateUserEmailFromId(userId)"
+	authCookie := http.Cookie{
+		Name:     "socialNetworkAuth",
+		Value:    "true",
+		Expires:  time.Now().Add(time.Minute * 500),
+		Path:     "/",
+		SameSite: http.SameSiteNoneMode,
+		Secure:   true,
+	}
+	http.SetCookie(w, &authCookie)
+	callback["login"] = "success"
+	callback["userid"] = "userId"
+	callback["useravatar"] = "validators.ValidateUserAvatar(userId)"
+	callback["useremail"] = "validators.ValidateUserEmailFromId(userId)"
 	// } else {
 	// 	callback["login"] = "fail"
 	// 	callback["error"] = userCookie
 	// }
 
-	writeData,_ := json.Marshal(callback)
+	writeData, _ := json.Marshal(callback)
 	// helpers.CheckErr("handleLogin", err)
 	w.Write(writeData)
+}
+
+func LoginHandler2(w http.ResponseWriter, r *http.Request) {
+
+	var user structs.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	fmt.Printf("LoginHandler user: %+v\n", user)
+
+	userID, verified := utils.VerifyUser(user)
+	if verified {
+		err := utils.SetUserOnline(userID)
+		if err != nil {
+			http.Error(w, "Error setting user online", http.StatusInternalServerError)
+			return
+		}
+		sessionUUID, err := utils.CreateSession(userID)
+		if err != nil {
+			http.Error(w, "Failed to create a session", http.StatusInternalServerError)
+			return
+		}
+		cookie := utils.CreateSessionCookie(sessionUUID)
+		http.SetCookie(w, cookie)
+		json.NewEncoder(w).Encode(map[string]interface{}{"success": true, "sessionUUID": sessionUUID})
+		// userIDWS = userID
+	} else {
+		json.NewEncoder(w).Encode(map[string]interface{}{"success": false, "message": "Wrong email or password"})
+	}
 }
