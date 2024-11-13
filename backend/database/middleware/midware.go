7f7eb56fb34d7bb53a13980bb294453ad5eee3cc
Miikael Volkonski <miikael@volkonski.com>
2024-11-13 08:05:57 +0200

    midware test wip

---
+++ backend/database/middleware/midware.go
@@ -0,0 +1,23 @@
+package middleware
+
+import "net/http"
+
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch origin := r.Header.Get("Origin"); origin {
+		case "http://localhost:3000",
+			"ws://localhost:3000":		
+			(w).Header().Set("Access-Control-Allow-Origin", origin)
+		}
+		(w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		(w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		(w).Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
--- backend/handlers/loginHandler.go
+++ backend/handlers/loginHandler.go
@@ -11,25 +11,16 @@ import (
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4000")
-    w.Header().Set("Access-Control-Allow-Credentials", "true")
-    w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-    // Handle preflight
-    if r.Method == "OPTIONS" {
-        w.WriteHeader(http.StatusOK)
-        return
-    }
-
 	var user structs.User
 
-	// FOR TESTING ----------
-	user.Password = "Password"
-	user.Username = "User"
-	user.Email = "User@email.com"
-	user.Identifier = "User"
-	// ----------------------
+	// // FOR TESTING ----------
+	// user.Password = "Password"
+	// user.Username = "User"
+	// user.Email = "User@email.com"
+	// user.Identifier = "User"
+	// // ----------------------
+
+	
 
 	json.NewDecoder(r.Body).Decode(&user)
 
