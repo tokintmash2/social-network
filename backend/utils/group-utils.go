be28a13b87fc83055f52a96f82593beeefe47496
Miikael Volkonski <miikael@volkonski.com>
2024-11-17 10:25:29 +0200

    group creator added to group memberships table

--- backend/utils/group-utils.go
+++ backend/utils/group-utils.go
@@ -7,7 +7,6 @@ import (
 )
 
 func CreateGroup(group structs.Group) error {
-
 	log.Println("Got group:", group)
 
 	tx, err := database.DB.Begin()
@@ -15,17 +14,36 @@ func CreateGroup(group structs.Group) error {
 		return err
 	}
 	defer tx.Rollback()
-	
+
 	// Insert group
-	_, err = tx.Exec(`
-	INSERT INTO groups (group_name, description, creator_id, created_at)
-	VALUES (?, ?, ?, ?)`,
-	group.Name, group.Description, group.CreatorID, group.CreatedAt,
+	result, err := tx.Exec(`
+        INSERT INTO groups (group_name, description, creator_id, created_at)
+        VALUES (?, ?, ?, ?)`,
+		group.Name, group.Description, group.CreatorID, group.CreatedAt,
 	)
 	if err != nil {
 		log.Println("Error inserting group:", err)
 		return err
 	}
+
+	// Get the newly created group ID
+	groupID, err := result.LastInsertId()
+	if err != nil {
+		log.Println("Error getting last insert ID:", err)
+		return err
+	}
+
+	// Add creator as admin member
+	_, err = tx.Exec(`
+        INSERT INTO group_memberships (group_id, user_id, role, joined_at)
+        VALUES (?, ?, 'admin', ?)`,
+		groupID, group.CreatorID, group.CreatedAt,
+	)
+	if err != nil {
+		log.Println("Error adding creator as admin:", err)
+		return err
+	}
+
 	if err = tx.Commit(); err != nil {
 		log.Printf("Error committing transaction: %v\n", err)
 		return err
@@ -33,3 +51,31 @@ func CreateGroup(group structs.Group) error {
 
 	return nil
 }
+
+// func CreateGroup(group structs.Group) error {
+
+// 	log.Println("Got group:", group)
+
+// 	tx, err := database.DB.Begin()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer tx.Rollback()
+
+// 	// Insert group
+// 	_, err = tx.Exec(`
+// 	INSERT INTO groups (group_name, description, creator_id, created_at)
+// 	VALUES (?, ?, ?, ?)`,
+// 	group.Name, group.Description, group.CreatorID, group.CreatedAt,
+// 	)
+// 	if err != nil {
+// 		log.Println("Error inserting group:", err)
+// 		return err
+// 	}
+// 	if err = tx.Commit(); err != nil {
+// 		log.Printf("Error committing transaction: %v\n", err)
+// 		return err
+// 	}
+
+// 	return nil
+// }
