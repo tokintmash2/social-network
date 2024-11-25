9251809a1cc96356f8ed930d616855fcf989920b
Miikael Volkonski <miikael@volkonski.com>
2024-11-25 15:48:47 +0200

    seems to work

--- backend/handlers/runHandlers.go
+++ backend/handlers/runHandlers.go
@@ -3,18 +3,17 @@ package handlers
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 )
 
 func RunHandlers(r *http.ServeMux) {
 
-	avatarPath, err := filepath.Abs("backend/avatars")
-	if err != nil {
-		panic("Could not resolve avatar directory path")
-	}
+	// avatarPath, err := filepath.Abs("backend/avatars")
+	// if err != nil {
+	// 	panic("Could not resolve avatar directory path")
+	// }
 
 	fmt.Println("Setting up file server for avatars at backend/avatars")
-	r.Handle("/avatars/", http.StripPrefix("/avatars/", http.FileServer(http.Dir(avatarPath))))
+	// r.Handle("/avatars/", http.StripPrefix("/avatars/", http.FileServer(http.Dir(avatarPath))))
 
 	r.HandleFunc("/login", LoginHandler)
 	r.HandleFunc("/register", SignupHandler)
@@ -26,4 +25,6 @@ func RunHandlers(r *http.ServeMux) {
 	r.HandleFunc("/posts", CreatePostHandler)
 	r.HandleFunc("/group-posts", CreateGroupPostHandler)
 	r.HandleFunc("/ws", HandleConnections)
+	r.Handle("/avatars/", http.StripPrefix("/avatars/", http.FileServer(http.Dir("./avatars"))))
+
 }
