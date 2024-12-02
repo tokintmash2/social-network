1a9109c893195b948e6eda40191e0413230b366f
Miikael Volkonski <miikael@volkonski.com>
2024-12-02 11:43:50 +0200

    random commit

--- backend/handlers/runHandlers.go
+++ backend/handlers/runHandlers.go
@@ -6,14 +6,6 @@ import (
 
 func RunHandlers(r *http.ServeMux) {
 
-	// avatarPath, err := filepath.Abs("backend/avatars")
-	// if err != nil {
-	// 	panic("Could not resolve avatar directory path")
-	// }
-
-	// fmt.Println("Setting up file server for avatars at backend/avatars")
-	// r.Handle("/avatars/", http.StripPrefix("/avatars/", http.FileServer(http.Dir(avatarPath))))
-
 	r.HandleFunc("/login", LoginHandler)
 	r.HandleFunc("/register", SignupHandler)
 	r.HandleFunc("/logout", LogoutHandler)
--- backend/utils/get-send-groups.go
+++ backend/utils/get-send-groups.go
@@ -101,31 +101,3 @@ func CreateGroup(group structs.Group) error {
 
 	return nil
 }
-
-// func CreateGroup(group structs.Group) error {
-
-// 	log.Println("Got group:", group)
-
-// 	tx, err := database.DB.Begin()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer tx.Rollback()
-
-// 	// Insert group
-// 	_, err = tx.Exec(`
-// 	INSERT INTO groups (group_name, description, creator_id, created_at)
-// 	VALUES (?, ?, ?, ?)`,
-// 	group.Name, group.Description, group.CreatorID, group.CreatedAt,
-// 	)
-// 	if err != nil {
-// 		log.Println("Error inserting group:", err)
-// 		return err
-// 	}
-// 	if err = tx.Commit(); err != nil {
-// 		log.Printf("Error committing transaction: %v\n", err)
-// 		return err
-// 	}
-
-// 	return nil
-// }
