a7bfda2ef4f9b516fbc935b0f05f8229c1891907
leiland <eilandliis@gmail.com>
2024-12-04 14:19:30 +0200

    register and login ui

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -19,7 +19,7 @@ export default function LoginPage() {
 		password: '',
 	})
 
-	const [buttonDisabled, setButtonDisabled] = React.useState(false)
+	const [buttonDisabled, setButtonDisabled] = React.useState(true)
 
 	useEffect(() => {
 		if (user.email.trim().length > 0 && user.password.trim().length > 0) {
@@ -32,7 +32,7 @@ export default function LoginPage() {
 	const [loading, setLoading] = React.useState(false)
 
 	const onLogin = async (e: React.FormEvent) => {
-		e.preventDefault() // Prevent form submission
+		e.preventDefault()
 		try {
 			setLoading(true)
 			const response = await axios.post('http://localhost:8080/login', user, {
@@ -54,7 +54,6 @@ export default function LoginPage() {
 	return (
 		<div className='min-h-screen bg-base-200 flex'>
 			<div className='w-2/5 flex items-center justify-center'>
-
 				<div>
 					<Toaster />
 				</div>
@@ -65,14 +64,12 @@ export default function LoginPage() {
 							SPHERE
 						</h1>
 
-
 						<form className='flex flex-col items-center justify-center'>
-							{/* Email */}
 							<label className='form-control w-2/3'>
-								
+								<span className='label-text text-xs mb-0.5 text-[#8DABC2] font-light'>Email</span>
 								<input
 									type='email'
-									className='input bg-white shadow-inner shadow-gray-400 rounded-2xl'
+									className='input h-9 bg-white shadow-inner shadow-gray-400 rounded-2xl text-[#687984] text-sm font-light'
 									value={user.email}
 									onChange={(e) =>
 										setUser({
@@ -83,11 +80,11 @@ export default function LoginPage() {
 								/>
 							</label>
 
-							{/* Password */}
 							<label className='form-control w-2/3 mt-6'>
+								<span className='label-text text-xs mb-0.5 text-[#8DABC2] font-light'>Password</span>
 								<input
 									type='password'
-									className='input bg-white shadow-inner shadow-gray-400 rounded-2xl'
+									className='input h-9 bg-white shadow-inner shadow-gray-400 rounded-2xl text-[#687984] text-sm font-light'
 									value={user.password}
 									onChange={(e) =>
 										setUser({
@@ -98,12 +95,11 @@ export default function LoginPage() {
 								/>
 							</label>
 
-							{/* Submit Button */}
 							<div className='form-control mt-6 w-2/3 item-center'>
 								<button
 									type='submit'
-									className='btn btn-primary bg-[#B9D7EA] border-0 shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-2xl text-[#687984] font-light hover:bg-[#A3B8C5] hover:text-[#FFFFFF]'
-
+									className='btn h-9 btn-primary bg-[#B9D7EA] border-0 shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-2xl text-[#687984] font-light hover:bg-[#A3B8C5] hover:text-[#FFFFFF]'
+									disabled={buttonDisabled}
 									onClick={onLogin}
 								>
 									{loading ? 'Logging in..' : 'Log in'}
@@ -111,10 +107,9 @@ export default function LoginPage() {
 							</div>
 						</form>
 
-						{/* Link to Register */}
 						<p className='text-center mt-4'>
 							Don&apos;t have an account yet?{' '}
-							<Link href='/register' className='text-primary text-[#B9D7EA] '>
+							<Link href='/register' className='text-primary text-[#8DABC2] '>
 								Register
 							</Link>
 						</p>
@@ -122,8 +117,7 @@ export default function LoginPage() {
 				</div>
 			</div>
 
-			{/* Right side - Background */}
-			<div className='w-3/5 bg-cover bg-center'  style={{ backgroundImage: 'url(https://res.cloudinary.com/dtdneratd/image/upload/v1733229108/augustine-wong-T0BYurbDK_M-unsplash-min_obmpdc.jpg)' }}></div>
+			<div className='w-3/5 bg-cover bg-center' style={{ backgroundImage: 'url(https://res.cloudinary.com/dtdneratd/image/upload/v1733229108/augustine-wong-T0BYurbDK_M-unsplash-min_obmpdc.jpg)' }}></div>
 		</div>
 	)
 }
