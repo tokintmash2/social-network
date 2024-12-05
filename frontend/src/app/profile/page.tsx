f388a38d4847dd7609b4556de688a2f40f3abea9
Liina-Maria Bakhoff <96503564+liinabakhoff@users.noreply.github.com>
2024-12-05 11:21:28 +0200

    middleware: verify user and define guarded pages

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -5,14 +5,22 @@ import { useLoggedInUser } from '../context/UserContext'
 
 // Show my profile page
 export default function MyProfilePage() {
-	const { loggedInUser } = useLoggedInUser()
+	const { loggedInUser, loading } = useLoggedInUser()
+
+	if (loading) {
+		return <div>Loading...</div>
+	}
 
 	return (
 		<div>
 			<Header />
-		<div className='container mx-auto'>
-			{loggedInUser && <UserData userId={loggedInUser.id} accessType='SELF' />}
-		</div>
+			<div className='container mx-auto'>
+				{loggedInUser ? (
+					<UserData userId={loggedInUser.id} accessType='SELF' />
+				) : (
+					<div>Please log in to view your profile.</div>
+				)}
+			</div>
 		</div>
 	)
 }
